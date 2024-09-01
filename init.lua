676df18139a1efcb8b7bf26a79d4d8cab503e218
Jordi Chulia <jorchube.dev@gmail.com>
2024-09-01 23:53:41 +0200

    Delete commented code

--- init.lua
+++ init.lua
@@ -862,21 +862,6 @@ require('lazy').setup({
     end,
   },
 --]]
-  --[[
-  {
-    'Shatur/neovim-ayu',
-    confir = function()
-      require('ayu').setup {
-        mirage = true, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
-        terminal = true, -- Set to `false` to let terminal manage its own colors.
-        overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
-      }
-    end,
-    init = function()
-      vim.cmd.colorscheme 'ayu-mirage'
-    end,
-  },
---]]
   {
     'catppuccin/nvim',
     name = 'catppuccin',
