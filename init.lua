e163afdbe20a66cbb0a29b5bf78ae804904e0397
Jordi Chulia <jchulia@mercadona.es>
2024-09-11 15:34:28 +0200

    Add total lines to status line

--- init.lua
+++ init.lua
@@ -919,7 +919,7 @@ require('lazy').setup({
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return '%2l/%2L:%-2v'
       end
 
       -- ... and there is more!
