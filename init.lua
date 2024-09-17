0b60c6ea41dc87c0ec9a0e033529bf2b0aa51d8f
Jordi Chulia <jchulia@mercadona.es>
2024-09-17 18:58:29 +0200

    Better use spacing when telescoping symbols

--- init.lua
+++ init.lua
@@ -572,11 +572,19 @@ require('lazy').setup({
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          map('<leader>ds', function()
+            require('telescope.builtin').lsp_document_symbols { symbol_width = 0.90, symbol_type_width = 0.10 }
+          end, '[D]ocument [S]ymbols')
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          map('<leader>ws', function()
+            require('telescope.builtin').lsp_dynamic_workspace_symbols {
+              fname_width = 0.60,
+              symbol_width = 0.30,
+              symbol_type_width = 0.10,
+            }
+          end, '[W]orkspace [S]ymbols')
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
