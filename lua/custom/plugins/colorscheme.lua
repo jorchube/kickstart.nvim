b4c8ba2b2b3216298c483124d5bea45603ce2524
Jordi Chulia <jorchube.dev@gmail.com>
2024-09-21 12:24:15 +0200

    Add some colorschemes and switch to darcula

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -2,8 +2,23 @@ return {
   {
     'catppuccin/nvim',
     name = 'catppuccin',
+  },
+  {
+    'Mofiqul/vscode.nvim',
+  },
+  {
+    'navarasu/onedark.nvim',
+  },
+  {
+    'EdenEast/nightfox.nvim',
+  },
+  {
+    'shaunsingh/nord.nvim',
+  },
+  {
+    'xiantang/darcula-dark.nvim',
     init = function()
-      vim.cmd.colorscheme 'catppuccin-frappe'
+      vim.cmd.colorscheme 'darcula-dark'
     end,
   },
 }
