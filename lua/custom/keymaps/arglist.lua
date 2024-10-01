b3b942e8b5208c4e7054e5aa3d4e77b353658d52
Jordi Chulia <jorchube.dev@gmail.com>
2024-10-02 00:07:43 +0200

    Tweak arglist shortcuts

--- lua/custom/keymaps/arglist.lua
+++ lua/custom/keymaps/arglist.lua
@@ -4,5 +4,11 @@ vim.keymap.set('n', '<leader>ax', ':argd<CR>', { desc = 'Delete current file fro
 vim.keymap.set('n', '<leader>aX', ':argd *<CR>', { desc = 'Clear arglist' })
 vim.keymap.set('n', ']a', ':n<CR>', { desc = 'Next arglist file' })
 vim.keymap.set('n', '[a', ':N<CR>', { desc = 'Previous arglist file' })
+vim.keymap.set('n', '<leader>1', ':1argu<CR>', { desc = 'Edit file 1 in arglist' })
+vim.keymap.set('n', '<leader>2', ':2argu<CR>', { desc = 'Edit file 2 in arglist' })
+vim.keymap.set('n', '<leader>3', ':3argu<CR>', { desc = 'Edit file 3 in arglist' })
+vim.keymap.set('n', '<leader>4', ':4argu<CR>', { desc = 'Edit file 4 in arglist' })
+vim.keymap.set('n', '<leader>5', ':5argu<CR>', { desc = 'Edit file 5 in arglist' })
+vim.keymap.set('n', '<leader>6', ':6argu<CR>', { desc = 'Edit file 6 in arglist' })
 
 -- vim: ts=2 sts=2 sw=2 et
