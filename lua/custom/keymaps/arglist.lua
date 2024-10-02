5e602b26c9f4b924b7e21d09d5a7333f94c0889f
Jordi Chulia <jchulia@mercadona.es>
2024-10-02 12:42:37 +0200

    Tweaks

--- lua/custom/keymaps/arglist.lua
+++ lua/custom/keymaps/arglist.lua
@@ -4,9 +4,9 @@ vim.keymap.set('n', '<leader>ax', ':argd<CR>', { desc = 'Delete current file fro
 vim.keymap.set('n', '<leader>aX', ':argd *<CR>', { desc = 'Clear arglist' })
 vim.keymap.set('n', ']a', ':n<CR>', { desc = 'Next arglist file' })
 vim.keymap.set('n', '[a', ':N<CR>', { desc = 'Previous arglist file' })
-vim.keymap.set('n', '<leader>1', ':1argu<CR>', { desc = 'Edit file 1 in arglist' })
-vim.keymap.set('n', '<leader>2', ':2argu<CR>', { desc = 'Edit file 2 in arglist' })
-vim.keymap.set('n', '<leader>3', ':3argu<CR>', { desc = 'Edit file 3 in arglist' })
-vim.keymap.set('n', '<leader>4', ':4argu<CR>', { desc = 'Edit file 4 in arglist' })
+vim.keymap.set('n', '<leader>1', ':1argu<CR>', { desc = 'File 1 in arglist' })
+vim.keymap.set('n', '<leader>2', ':2argu<CR>', { desc = 'File 2 in arglist' })
+vim.keymap.set('n', '<leader>3', ':3argu<CR>', { desc = 'File 3 in arglist' })
+vim.keymap.set('n', '<leader>4', ':4argu<CR>', { desc = 'File 4 in arglist' })
 
 -- vim: ts=2 sts=2 sw=2 et
